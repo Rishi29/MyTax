cde9e4d2e8eb90ec3bf833ec4a883fdecbbf54ed
Rishikesh Mandal <me.rishi29@gmail.com>
2020-03-17 21:04:38 -0400

    First commit

---
+++ tax-range-web-services/src/main/java/com/taxrange/rest/webservices/taxrangewebservices/TaxRangeWebServicesApplication.java
@@ -0,0 +1,13 @@
+package com.taxrange.rest.webservices.taxrangewebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxRangeWebServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaxRangeWebServicesApplication.class, args);
+	}
+
+}
